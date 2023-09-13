1f5aedf40891a4204c3abb7786aed47bb12f7f02
KuriZd <KuriZd@protonmail.com>
2023-09-12 22:31:23 -0600

    Calculadora

---
+++ labuena/src/main/java/com/mycompany/labuena/Labuena.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.labuena;
+
+/**
+ *
+ * @author zamud
+ */
+public class Labuena {
+
+    public static void main(String[] args) {
+        new cal().setVisible(true);
+    }
+}
